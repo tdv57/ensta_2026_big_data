3e0f407f206fdf0a51dcedf804896f9e22649cef
root <root@ORDI-Ferdi>
2026-03-23 11:51:39 +0100

    Supprime les anciens fichiers inutiles

--- read_wat_parquet_files.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark_session = SparkSession.builder.getOrCreate()
-
-df = spark_session.read.parquet("wat_parquet/wat_parquet_files_0_1.parquet")
-
-df.show(100)
-print(df.count())
