8161081f3c551027f09a73d48f0be958b8516bd9
root <root@ORDI-Ferdi>
2026-03-24 09:34:27 +0100

    modif de fichiers

--- python/write_final_parquet.py
+++ python/write_final_parquet.py
@@ -2,29 +2,65 @@ from pyspark.sql import SparkSession
 import os 
 from LOG_MESSAGE import INFO, DEBUG, ERROR
 
+
+
 wet_parquet = os.path.abspath("wet_parquet")
 wat_parquet = os.path.abspath("wat_parquet")
 
-spark_session = SparkSession.builder \
-    .getOrCreate()
+def write_final_parquet_files(spark_session, first_url_processed_in_parquet,last_url_processed_in_parquet, pas):
+    spark_session = SparkSession.builder \
+        .getOrCreate()
 
-pas = 100000
-for i in range(0,900000,pas):
-    url = str(i) + "_" + str(i+pas)
-#df = spark.read.option("recursiveFileLookup", "true").parquet("wat_parquet/")
-    wet_parquet = f"wet_parquet/wet_parquet_files_{url}.parquet"
-    wat_parquet = f"wat_parquet/wat_parquet_files_{url}.parquet"
+    pas = 100000
+    first_url_processed_in_parquet = 0
+    last_url_processed_in_parquet = 900000
+    for i in range(first_url_processed_in_parquet,last_url_processed_in_parquet,pas):
+        url = str(i) + "_" + str(i+pas)
+    #df = spark.read.option("recursiveFileLookup", "true").parquet("wat_parquet/")
+        wet_parquet = f"wet_parquet/wet_parquet_files_{url}.parquet"
+        wat_parquet = f"wat_parquet/wat_parquet_files_{url}.parquet"
 
-    df_wet = spark_session.read.parquet(wet_parquet)
+        df_wet = spark_session.read.parquet(wet_parquet)
 
-    df_wat = spark_session.read.parquet(wat_parquet)
+        df_wat = spark_session.read.parquet(wat_parquet)
 
-    df_wet = df_wet.drop("warc_id")
-    df_wat = df_wat.drop("warc_id")
+        df_wet = df_wet.drop("warc_id")
+        df_wat = df_wat.drop("warc_id")
 
-    df_joined = df_wet.join(df_wat, on="WARC_REFERS_TO", how="inner")
+        df_joined = df_wet.join(df_wat, on="WARC_REFERS_TO", how="inner")
 
-    df_joined.write.mode("overwrite").parquet(f"final_parquet/final_parquet_files_{url}.parquet")
-    print(INFO + f"final parquet écrit pour url = {url}")
+        df_joined.write.mode("overwrite").parquet(f"final_parquet/final_parquet_files_{url}.parquet")
+        print(INFO + f"final parquet écrit pour url = {url}")
 
-df_joined.show(10)
+    df_joined.show(10)
+    
+def main(): 
+
+    spark_session = SparkSession.builder \
+        .getOrCreate()
+
+    pas = 100000
+    first_url_processed_in_parquet = 0
+    last_url_processed_in_parquet = 900000
+    for i in range(first_url_processed_in_parquet,last_url_processed_in_parquet,pas):
+        url = str(i) + "_" + str(i+pas)
+    #df = spark.read.option("recursiveFileLookup", "true").parquet("wat_parquet/")
+        wet_parquet = f"wet_parquet/wet_parquet_files_{url}.parquet"
+        wat_parquet = f"wat_parquet/wat_parquet_files_{url}.parquet"
+
+        df_wet = spark_session.read.parquet(wet_parquet)
+
+        df_wat = spark_session.read.parquet(wat_parquet)
+
+        df_wet = df_wet.drop("warc_id")
+        df_wat = df_wat.drop("warc_id")
+
+        df_joined = df_wet.join(df_wat, on="WARC_REFERS_TO", how="inner")
+
+        df_joined.write.mode("overwrite").parquet(f"final_parquet/final_parquet_files_{url}.parquet")
+        print(INFO + f"final parquet écrit pour url = {url}")
+
+    df_joined.show(10)
+
+if __name__ == "__main__":
+    main()
